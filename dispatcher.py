8bee40b0adf1efd8c83aa54dcaa9d8a735999264
camenduru <camenduru@gmail.com>
2024-05-24 01:52:34 +0000

    test

--- dispatcher.py
+++ dispatcher.py
@@ -60,12 +60,11 @@ def loop():
 
                     if response and response.status_code == 200:
                         try:
-                            payload = {"jobId": str(job_id), "result": response.json()['attachments'][0]['url']}
-                            # if isinstance(result, str):
-                            #     payload = {"jobId": str(job_id), "result": response.json()['attachments'][0]['url']}
-                            # else:
-                            #     urls = [attachment['url'] for attachment in response.json()['attachments']]
-                            #     payload = {"jobId": str(job_id), "result": str(urls)}
+                            if isinstance(result, str):
+                                payload = {"jobId": str(job_id), "result": response.json()['attachments'][0]['url']}
+                            else:
+                                urls = [attachment['url'] for attachment in response.json()['attachments']]
+                                payload = {"jobId": str(job_id), "result": str(urls)}
                             requests.post(f"{web_uri}/api/notify", data=json.dumps(payload), headers={'Content-Type': 'application/json', "authorization": f"{web_token}"})
                         except Exception as e:
                             print(f"An unexpected error occurred: {e}")
