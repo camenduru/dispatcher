039b3e8c89519c8c0fe70cc67434ab24f18cc42e
camenduru <camenduru@gmail.com>
2024-06-03 05:41:38 +0000

    timer

--- dispatcher.py
+++ dispatcher.py
@@ -1,5 +1,9 @@
-import http.server, socketserver, os, json
-import requests, base64, threading
+import http.server
+import socketserver
+import os
+import json
+import requests
+import threading
 from pymongo import MongoClient
 
 mongodb_uri = os.getenv('com_camenduru_mongodb_uri')
@@ -12,77 +16,79 @@ web_uri = os.getenv('com_camenduru_web_uri')
 web_token = os.getenv('com_camenduru_web_token')
 
 def loop():
-  client = MongoClient(mongodb_uri)
-  db = client['web']
-  collection_job = db['job']
-  collection_detail = db['detail']
+    client = MongoClient(mongodb_uri)
+    db = client['web']
+    collection_job = db['job']
+    collection_detail = db['detail']
 
-  while True:
-    waiting_documents = collection_job.find({"$and":[ {"status":"WAITING"}, {"source":job_source}]})
-    for waiting_document in waiting_documents:
-        server = waiting_document['type']
-        if(server==job_type):
-            login = waiting_document['login']
-            detail = collection_detail.find_one({"login": login})
-            command = waiting_document['command']
-            source_channel = waiting_document['source_channel']
-            source_id = waiting_document['source_id']
-            job_id = waiting_document['_id']
-            if int(detail['total']) > 0:
-                collection_job.update_one({"_id": job_id}, {"$set": {"status": "WORKING"}})
-                try:
-                    from gradio_client import Client
-                    client = Client(worker_uri, verbose=False)
-                    result = client.predict(command, fn_index=0)
-
-                    if isinstance(result, str):
-                        file_path = result
-                        default_filename = os.path.basename(file_path)
-                        files = {default_filename: open(file_path, "rb").read()}
-                    elif isinstance(result, dict):
-                        file_path = result.get('video')
-                        default_filename = os.path.basename(file_path)
-                        files = {default_filename: open(file_path, "rb").read()}
-                    elif isinstance(result, tuple):
-                        first_key = next(iter(result[0]))
-                        file_path = result[0][first_key]
-                        file_paths = result[1]
-                        default_filename = os.path.basename(file_path)
-                        files = { default_filename: open(file_path, "rb").read() }
-                        for path in file_paths:
-                            filename = os.path.basename(path)
-                            with open(path, "rb") as file:
-                                files[filename] = file.read()
-                    
-                    payload = {"content": f"{command} <@{source_id}>"}
-                    response = None
+    def check_jobs():
+        waiting_documents = collection_job.find({"$and":[ {"status":"WAITING"}, {"source":job_source}]})
+        for waiting_document in waiting_documents:
+            server = waiting_document['type']
+            if(server==job_type):
+                login = waiting_document['login']
+                detail = collection_detail.find_one({"login": login})
+                command = waiting_document['command']
+                source_channel = waiting_document['source_channel']
+                source_id = waiting_document['source_id']
+                job_id = waiting_document['_id']
+                if int(detail['total']) > 0:
+                    collection_job.update_one({"_id": job_id}, {"$set": {"status": "WORKING"}})
                     try:
-                        response = requests.post(f"https://discord.com/api/v9/channels/{source_channel}/messages", data=payload, headers={"authorization": f"Bot {discord_token}"}, files=files)
-                        response.raise_for_status()
-                    except Exception as e:
-                        print(f"Discord an unexpected error occurred: {e}")
+                        from gradio_client import Client
+                        client = Client(worker_uri, verbose=False)
+                        result = client.predict(command, fn_index=0)
 
-                    if response and response.status_code == 200:
+                        if isinstance(result, str):
+                            file_path = result
+                            default_filename = os.path.basename(file_path)
+                            files = {default_filename: open(file_path, "rb").read()}
+                        elif isinstance(result, dict):
+                            file_path = result.get('video')
+                            default_filename = os.path.basename(file_path)
+                            files = {default_filename: open(file_path, "rb").read()}
+                        elif isinstance(result, tuple):
+                            first_key = next(iter(result[0]))
+                            file_path = result[0][first_key]
+                            file_paths = result[1]
+                            default_filename = os.path.basename(file_path)
+                            files = { default_filename: open(file_path, "rb").read() }
+                            for path in file_paths:
+                                filename = os.path.basename(path)
+                                with open(path, "rb") as file:
+                                    files[filename] = file.read()
+                        
+                        payload = {"content": f"{command} <@{source_id}>"}
+                        response = None
                         try:
-                            if isinstance(result, str):
-                                payload = {"jobId": str(job_id), "result": response.json()['attachments'][0]['url']}
-                            elif isinstance(result, dict):
-                                payload = {"jobId": str(job_id), "result": response.json()['attachments'][0]['url']}
-                            elif isinstance(result, tuple):
-                                urls = [attachment['url'] for attachment in response.json()['attachments']]
-                                payload = {"jobId": str(job_id), "result": str(urls)}
-                            requests.post(f"{web_uri}/api/notify", data=json.dumps(payload), headers={'Content-Type': 'application/json', "authorization": f"{web_token}"})
+                            response = requests.post(f"https://discord.com/api/v9/channels/{source_channel}/messages", data=payload, headers={"authorization": f"Bot {discord_token}"}, files=files)
+                            response.raise_for_status()
                         except Exception as e:
-                            print(f"An unexpected error occurred: {e}")
+                            print(f"Discord an unexpected error occurred: {e}")
 
-                except Exception as e:
-                    print(f"Client an unexpected error occurred: {e}")
-            else:
-                try:
-                    payload = {"jobId": str(job_id), "result": "Oops! Your balance is insufficient. Please redeem a Tost wallet code."}
-                    requests.post(f"{web_uri}/api/notify", data=json.dumps(payload), headers={'Content-Type': 'application/json', "authorization": f"{web_token}"})
-                except Exception as e:
-                    print(f"An unexpected error occurred: {e}")
+                        if response and response.status_code == 200:
+                            try:
+                                if isinstance(result, str):
+                                    payload = {"jobId": str(job_id), "result": response.json()['attachments'][0]['url']}
+                                elif isinstance(result, dict):
+                                    payload = {"jobId": str(job_id), "result": response.json()['attachments'][0]['url']}
+                                elif isinstance(result, tuple):
+                                    urls = [attachment['url'] for attachment in response.json()['attachments']]
+                                    payload = {"jobId": str(job_id), "result": str(urls)}
+                                requests.post(f"{web_uri}/api/notify", data=json.dumps(payload), headers={'Content-Type': 'application/json', "authorization": f"{web_token}"})
+                            except Exception as e:
+                                print(f"An unexpected error occurred: {e}")
+
+                    except Exception as e:
+                        print(f"Client an unexpected error occurred: {e}")
+                else:
+                    try:
+                        payload = {"jobId": str(job_id), "result": "Oops! Your balance is insufficient. Please redeem a Tost wallet code."}
+                        requests.post(f"{web_uri}/api/notify", data=json.dumps(payload), headers={'Content-Type': 'application/json', "authorization": f"{web_token}"})
+                    except Exception as e:
+                        print(f"An unexpected error occurred: {e}")
+        threading.Timer(1, check_jobs).start()
+    check_jobs()
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def translate_path(self, path):
@@ -101,5 +107,4 @@ Handler.extensions_map.update({
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     thread = threading.Thread(target=loop)
     thread.start()
-    print(f"Server running on port {PORT}")
     httpd.serve_forever()
--- dispatcher_runpod.py
+++ dispatcher_runpod.py
@@ -1,5 +1,9 @@
-import http.server, socketserver, os, json
-import requests, base64, threading
+import http.server
+import socketserver
+import os
+import json
+import requests
+import threading
 from pymongo import MongoClient
 
 mongodb_uri = os.getenv('com_camenduru_mongodb_uri')
@@ -12,43 +16,45 @@ web_uri = os.getenv('com_camenduru_web_uri')
 web_token = os.getenv('com_camenduru_web_token')
 
 def loop():
-  client = MongoClient(mongodb_uri)
-  db = client['web']
-  collection_job = db['job']
-  collection_detail = db['detail']
+    client = MongoClient(mongodb_uri)
+    db = client['web']
+    collection_job = db['job']
+    collection_detail = db['detail']
 
-  while True:
-    waiting_documents = collection_job.find({"$and":[ {"status":"WAITING"}, {"source":job_source}]})
-    for waiting_document in waiting_documents:
-        server = waiting_document['type']
-        if(server==job_type):
-            login = waiting_document['login']
-            detail = collection_detail.find_one({"login": login})
-            command = waiting_document['command']
-            source_channel = waiting_document['source_channel']
-            source_id = waiting_document['source_id']
-            job_id = waiting_document['_id']
-            if int(detail['total']) > 0:
-                collection_job.update_one({"_id": job_id}, {"$set": {"status": "WORKING"}})
-                command_data = json.loads(command)
-                command_data["source_id"] = source_id
-                command_data["source_channel"] = source_channel
-                command_data['job_id'] = str(job_id)
-                data = { "input": command_data }
-                headers = {
-                    "Content-Type": "application/json",
-                    "Authorization": f"Bearer {runpod_token}"
-                }
-                try:
-                    requests.post(worker_uri, headers=headers, json=data)
-                except Exception as e:
-                    print(f"An unexpected error occurred: {e}")
-            else:
-                try:
-                    payload = {"jobId": str(job_id), "result": "Oops! Your balance is insufficient. Please redeem a Tost wallet code."}
-                    requests.post(f"{web_uri}/api/notify", data=json.dumps(payload), headers={'Content-Type': 'application/json', "authorization": f"{web_token}"})
-                except Exception as e:
-                    print(f"An unexpected error occurred: {e}")
+    def check_jobs():
+        waiting_documents = collection_job.find({"$and":[ {"status":"WAITING"}, {"source":job_source}]})
+        for waiting_document in waiting_documents:
+            server = waiting_document['type']
+            if(server==job_type):
+                login = waiting_document['login']
+                detail = collection_detail.find_one({"login": login})
+                command = waiting_document['command']
+                source_channel = waiting_document['source_channel']
+                source_id = waiting_document['source_id']
+                job_id = waiting_document['_id']
+                if int(detail['total']) > 0:
+                    collection_job.update_one({"_id": job_id}, {"$set": {"status": "WORKING"}})
+                    command_data = json.loads(command)
+                    command_data["source_id"] = source_id
+                    command_data["source_channel"] = source_channel
+                    command_data['job_id'] = str(job_id)
+                    data = { "input": command_data }
+                    headers = {
+                        "Content-Type": "application/json",
+                        "Authorization": f"Bearer {runpod_token}"
+                    }
+                    try:
+                        requests.post(worker_uri, headers=headers, json=data)
+                    except Exception as e:
+                        print(f"An unexpected error occurred: {e}")
+                else:
+                    try:
+                        payload = {"jobId": str(job_id), "result": "Oops! Your balance is insufficient. Please redeem a Tost wallet code."}
+                        requests.post(f"{web_uri}/api/notify", data=json.dumps(payload), headers={'Content-Type': 'application/json', "authorization": f"{web_token}"})
+                    except Exception as e:
+                        print(f"An unexpected error occurred: {e}")
+        threading.Timer(1, check_jobs).start()
+    check_jobs()
 
 class MyHandler(http.server.SimpleHTTPRequestHandler):
     def translate_path(self, path):
@@ -67,5 +73,4 @@ Handler.extensions_map.update({
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     thread = threading.Thread(target=loop)
     thread.start()
-    print(f"Server running on port {PORT}")
     httpd.serve_forever()
